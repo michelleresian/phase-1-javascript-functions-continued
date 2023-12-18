47604568d430c5e1cdea70df64f2538d4af9460b
Michelle Resian <you@example.com>
2023-12-18 12:12:26 +0300

    extended functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+console.log(saturdayFun);
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective = 'special') {
+        return `You are ${symbol}${adjective}${symbol}!`;
+        console.log(`${part1} ${symbol}`)
+    }
+}
